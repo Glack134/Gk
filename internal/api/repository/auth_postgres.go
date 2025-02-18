40bfccd8709296106a17eb8cd5288f351f0f8ff9
Midiy <polyk6859@gmail.com>
2025-02-18 10:56:13 +0300

    update html for login

--- internal/api/repository/auth_postgres.go
+++ internal/api/repository/auth_postgres.go
@@ -18,9 +18,9 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 func (r *AuthPostgres) CreateUser(user model.User) (int, error) {
 	var id int
 
-	query := fmt.Sprintf(`INSERT INTO %s (username, password_hash, email) VALUES ($1, $2, $3) RETURNING id`, usersTable)
+	query := fmt.Sprintf(`INSERT INTO %s (country, username, password_hash, email) VALUES ($1, $2, $3, $4) RETURNING id`, usersTable)
 
-	row := r.db.QueryRow(query, user.Username, user.Password, user.Email)
+	row := r.db.QueryRow(query, user.Сountry, user.Username, user.Password, user.Email)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}
--- internal/model/user.go
+++ internal/model/user.go
@@ -2,6 +2,7 @@ package model
 
 type User struct {
 	Id       int    `json:"-" db:"id"`
+	Сountry  string `json:"country" binding:"required"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Email    string `json:"email" binding:"required"`
